e12c343476982479d9f2d386598d1a3a1818caf6
christian-oliveira <christianoliveirati@gmail.com>
2020-11-11 17:55:19 -0300

    :construction: funcionalidade de cadastro de venda em fase final

--- vendas/serializers.py
+++ vendas/serializers.py
@@ -1,43 +1,71 @@
 from rest_framework import serializers
 
+from produtos.serializers import ProdutoSerializer
 from .models import Venda, ItemVenda
+
+class ItemVendaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemVenda
+        fields = (
+            'produto',
+            'venda',
+            'quantidade',
+            'preco_unitario',
+            'preco_total',
+        )
+
+        read_only_fields = ['venda']
 
 
 class VendaCreateSerializer(serializers.ModelSerializer):
+    produtos = ItemVendaSerializer(many=True, required=True)
 
     class Meta:
         model = Venda
-        fields = [
+        fields = (
             'cliente',
             'vendedor',
+            'caixa',
             'produtos',
             'valor_total',
             'desconto',
-            'preco_final',
             'forma_pagamento',
+            'valor_pago',
             'qtd_parcelas',
             'valor_parcelas',
-            'venc_parcelas'
-        ]
+            'venc_parcelas',
+            'status'
+        )
+
+    def create(self, validated_data):
+        produtos_venda = validated_data.pop('produtos')
+        venda = Venda.objects.create(**validated_data)
+        produtos = [ItemVenda(venda=venda, **item) for item in produtos_venda]
+        ItemVenda.objects.bulk_create(produtos)
+        return venda
+            
 
 
 class VendaSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Venda
-        fields = [
+        fields = (
             'id',
             'cliente',
             'vendedor',
+            'caixa',
+            'produtos',
             'valor_total',
             'desconto',
-            'preco_final',
             'forma_pagamento',
+            'valor_pago',
             'qtd_parcelas',
             'valor_parcelas',
             'venc_parcelas',
+            'status',
             'criado_em',
             'atualizado_em',
             'ativo'
-        ]
+        )
         read_only_fields = ['id', 'criado_em', 'atualizado_em']
